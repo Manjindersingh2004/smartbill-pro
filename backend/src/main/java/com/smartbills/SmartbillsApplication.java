735fc4d669c669df8e1380c1973d2789b975a00e
Manjinder Singh <manjindersingh1212004@gmail.com>
2026-05-24 22:14:31 +0530

    Project Setup

---
+++ backend/src/main/java/com/smartbills/SmartbillsApplication.java
@@ -0,0 +1,13 @@
+package com.smartbills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartbillsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/smartbills/SmartbillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
